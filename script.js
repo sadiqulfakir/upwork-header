9ee0db62b803959ea9b73b405199c469ea8e1984
sadiqulfakir <sadikul0140@gmail.com>
2022-02-01 09:16:34 +0600

    updated

---
+++ script.js
@@ -0,0 +1,22 @@
+
+		function sidebarHandler(e) {
+			const sideBar = document.getElementById('sidebar').classList.toggle('active');
+		};
+		function findTalentHandler(e) {
+			const sideBar = document.getElementById('find-talent').classList.toggle('active');
+		};
+		function findWorkHandler(e) {
+			const sideBar = document.getElementById('find-work').classList.toggle('active');
+		};
+		function whyUpworkHandler(e) {
+			const sideBar = document.getElementById('why-upwork').classList.toggle('active');
+		};
+		function jobHandler(e) {
+			const sideBar = document.getElementById('sidebar-two').classList.toggle('active');
+		};
+		function projectHandler(e) {
+			const sideBar = document.getElementById('sidebar-three').classList.toggle('active');
+		};
+		function LetusHandler(e) {
+			const sideBar = document.getElementById('sidebar-four').classList.toggle('active');
+		};
